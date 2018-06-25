c804f90d23555544bc70ad934b5482d12dfe76e5
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-06-25 23:37:59 +0200

    add import and types of actions

--- actions.js
+++ actions.js
@@ -1,6 +1,36 @@
+import uuid from uuid;
+
 const ADD_COMMENT = 'ADD_COMMENT';
 const EDIT_COMMENT = 'EDIT_COMMENT';
 const REMOVE_COMMENT = 'REMOVE_COMMENT';
 const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
 const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
 
+{
+	type: ADD_COMMENT,
+	id: 1,
+	text: 'My first comment!' 
+}
+
+{
+	type: EDIT_COMMENT,
+	id: 2,
+	text: 'Edit text'
+}
+
+{
+	type: REMOVE_COMMENT,
+	id: 19,
+}
+
+{
+	type: THUMB_UP_COMMENT,
+	id: 13,
+	text: 'Add like'
+}
+
+{
+	type: THUMB_DOWN_COMMENT,
+	id: 14,
+	text: 'Add unlike'
+}
